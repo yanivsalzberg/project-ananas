da6867df64f79f37d58983363b75fe281f4188da
Arkadi <mrarkadi@gmail.com>
2017-03-15 00:25:17 +0200

    Functional controller + factory + mongoose database

--- public/js/services/pineAppFactory.js
+++ public/js/services/pineAppFactory.js
@@ -1,3 +1,16 @@
 app.factory('pineAppService', function($http) {
+
+var pineAppService = {};
+
+  pineAppService.getPineboxes = function() {
+    return $http.get('/pineboxes')
+      .then(function(response) {
+        console.log(response.data);
+        return response.data;
+      }, function(err) {
+          console.error(err);
+      });
+  };
+
   return pineAppService;
 });
