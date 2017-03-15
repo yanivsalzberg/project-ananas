55e0bd22069d693f9f046f624ae6e4447247dc1a
Arik92 <arik92@gmail.com>
2017-03-15 14:04:43 +0200

    trying to make one touch color toggle work

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -6,26 +6,40 @@ app.controller('pineAppCtrl', ['$scope', 'pineAppService', function($scope, pine
       alert("you win the game");
     }//if
   } //checkWin
+  $scope.changeColor = function(pBox) {
+    if ($scope.clicked){
+      pBox.display="navy";
+    } else {
+    pBox.display = pBox.color;
+  }
+  }//changeColor
   $scope.restoreDefaults = function(){
     $scope.click = false;
     //
   }//restore default setting-
-  $scope.play = function(pBox) {
+  $scope.play = function(pBox) {    
     if ($scope.clicked) {
+      $scope.changeColor(pBox);
       //$scope.changeColorWithTimer(pBox.color)
       if (pBox.color===$scope.selectedColor) {
         //$scope.vanishBoxes(pBox.id, selectedId); make them disappear with ng-show/css
         $scope.checkWin();
-      } else {
-       $scope.restoreDefaults();
-      }// else not of the same color
+      }//if same color
+      $scope.restoreDefaults();
     } else {
     $scope.clicked = true;
     $scope.selectedId = pBox.id;
     $scope.selectedColor = pBox.color;
-    // $scope.changeColor(pBox);
+    $scope.changeColor(pBox);
+
     } //else click
   } //play
+  $scope.arrange = function(arr) {
+    for (i = 0;i<arr.length;i++) {
+      arr[i].id = i;
+      arr[i].display="navy";
+    } //for i
+  }//arrange
 
 
   $scope.doubleArray = function(arr) {
@@ -34,9 +48,11 @@ app.controller('pineAppCtrl', ['$scope', 'pineAppService', function($scope, pine
     for (var i = 0;i<arr.length;i++) {
       res[resIndex] = arr[i];
       res[resIndex].id = resIndex;
+      res[resIndex].display= "navy";
       resIndex++;
       res[resIndex] = arr[i];
       res[resIndex].id = resIndex;
+      res[resIndex].display= "navy";
       resIndex++;
     } //for i
     return res;
@@ -50,14 +66,16 @@ app.controller('pineAppCtrl', ['$scope', 'pineAppService', function($scope, pine
     } //while
     return randomArr;
   }// randomize
-  $scope.pineboxes = [{color:"green"}, {color:"indigo"}, {color:"black"}, {color:"orange"}];
+  /*$scope.pineboxes = [{color:"green"}, {color:"indigo"}, {color:"black"}, {color:"orange"}];
   console.log($scope.pineboxes);
   $scope.numPairs = ($scope.pineboxes.length)*2;
   $scope.pineboxes = $scope.doubleArray($scope.pineboxes);
-  $scope.randomize($scope.pineboxes);
-  // pineAppService.getPineboxes().then(function(pineboxes) {
-  //   $scope.pineboxes = pineboxes;
-  //   $scope.numPairs = pineboxes.length/2; // alternatively: the db would contain only single values and a function would double the array and randomize it
-  //   console.log("num of pairs is: " + $scope.numPairs);
-  // });
+  $scope.randomize($scope.pineboxes); */
+  pineAppService.getPineboxes().then(function(pineboxes) {
+    //$scope.pineboxes = $scope.doubleArray(pineboxes);
+    $scope.pineboxes = $scope.randomize(pineboxes);
+    $scope.arrange(pineboxes);
+    $scope.numPairs = $scope.pineboxes.length/2; // alternatively: the db would contain only single values and a function would double the array and randomize it
+    console.log("num of pairs is: " + $scope.numPairs);
+  });
 }]);
