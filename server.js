c24dd25b5f1e5b631fe018615c1a40377baf02e4
Arik92 <arik92@gmail.com>
2017-04-05 12:44:23 +0300

    added database into heroku app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var pineRoutes = require('./routers/pine-router');
 var playerRoutes = require('./routers/player-router');
 var app = express();
 
-mongoose.connect("mongodb://localhost/pineboxes");
+mongoose.connect(process.env.CONNECTION_STRING||"mongodb://localhost/pineboxes");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,11 +14,10 @@ app.use('/players', playerRoutes);
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-
 app.all('*', function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(8000, function() {
-  console.log("Pineapple! Listening on 8000.");
+app.listen(process.env.PORT|| '8000', function() {
+  console.log("Pineapple! Listening on server.");
 });
